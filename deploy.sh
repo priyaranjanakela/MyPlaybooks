dc1e916680415818d96e6af2e4418d39fab675aa
priyaranjanakela <priyranjans@gmail.com>
2021-05-29 15:16:04 -0400

    updating script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for i in "$@"
-do
-curl --digest -L -D - {{ $i }} --header "Content-Type: application/json" -u akela:Gibi@1234 -d '{"operation" : "add", "address" : {"deployment" : "SampleWebApp.war"}, "content" : [{"url" : "file:/tmp/SampleWebApp.war"}],"json.pretty":1}
-done
+       do
+                curl --digest -L -D - {{ $i }} --header "Content-Type: application/json" -u akela:Gibi@1234 -d '{"operation" : "add", "address" : {"deployment" : "SampleWebApp.war"}, "content" : [{"url" : "file:/tmp/SampleWebApp.war"}],"json.pretty":1}'
+
+       done
